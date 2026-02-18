94586dbe1232ef7873dcb095a42380cebb10037f
Kiran Kumar <130830242+konetikirankumar@users.noreply.github.com>
2026-02-18 07:58:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const logger = require('./log');
+//const logger = require('./log');
 const express = require('express');
 const cheerio = require('cheerio');
 let got;
@@ -144,5 +144,6 @@ app.get('/api/download', async (req, res) => {
 //app.listen(3000, () => console.log('Server running on port 3000'));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
